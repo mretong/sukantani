55d798224a3ee5a4fd6980612dc6884e9f713248
Suhairi Abdul Hamid <suhairi81@gmail.com>
2016-12-14 12:07:05 +0800

    20161214 2

--- database/seeds/PesertaTableSeeder.php
+++ database/seeds/PesertaTableSeeder.php
@@ -17,23 +17,23 @@ class PesertaTableSeeder extends Seeder
     public function run()
     {
 
-    	factory(App\Peserta::class, 2500)->create();
+    	// factory(App\Peserta::class, 2500)->create();
 	
-    	$pesertas = Peserta::all();
+    	// $pesertas = Peserta::all();
 
-    	foreach($pesertas as $peserta) {
+    	// foreach($pesertas as $peserta) {
 
-    		$acara = Array();
-    		for($i=0; $i<rand(1, 3); $i++) {
-    			$randomAcara = rand(1, 20);
+    	// 	$acara = Array();
+    	// 	for($i=0; $i<rand(1, 3); $i++) {
+    	// 		$randomAcara = rand(1, 20);
 
-    			if(!in_array($randomAcara, $acara))
-    				array_push($acara, $randomAcara);
-    		}
+    	// 		if(!in_array($randomAcara, $acara))
+    	// 			array_push($acara, $randomAcara);
+    	// 	}
 
-    		$peserta->acara()->attach($acara);
+    	// 	$peserta->acara()->attach($acara);
     		
-    	}
+    	// }
         
     }
 
