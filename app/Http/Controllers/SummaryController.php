a48d1bb312839a523150f5eabf177cc9cf712ffd
Suhairi Abdul Hamid <suhairi81@gmail.com>
2016-12-21 09:46:27 +0800

    20161221 2

--- app/Http/Controllers/SummaryController.php
+++ app/Http/Controllers/SummaryController.php
@@ -4,11 +4,54 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Agensi;
+use App\Peserta;
+use App\Kontinjen;
+use App\Acara;
+
 class SummaryController extends Controller
 {
     public function index() {
 
+    	$agencies = Agensi::orderBy('id', 'asc')->get();
 
-    	return view('members.summary');
+    	// dd($agencies);
+
+    	// 1. kontinjen
+    	// 2. peserta
+
+    	$expect = 213;
+    	$collections = collect();
+
+    	foreach($agencies as $agency) {
+
+    		$kontinjen 			= Kontinjen::where('agensi_id', $agency->id)->count();
+    		$kontinjenLelaki	= Kontinjen::where('agensi_id', $agency->id)
+    								->where('jantina', 'LELAKI')
+    								->count();
+			$kontinjenWanita	= Kontinjen::where('agensi_id', $agency->id)
+    								->where('jantina', 'WANITA')
+    								->count();
+			$peserta 			= Peserta::where('agensi_id', $agency->id)->count();
+			$pesertaLelaki 		= Peserta::where('agensi_id', $agency->id)
+									->where('jantina', 'LELAKI')
+									->count();
+			$pesertaWanita 		= Peserta::where('agensi_id', $agency->id)
+									->where('jantina', 'WANITA')
+									->count();
+
+    		$collections = $collections->push([
+    							'agensi' 			=> $agency->nama,
+    							'agensiKod'			=> $agency->kod,
+    							'kontinjen' 		=> $kontinjen,
+    							'kontinjenLelaki' 	=> $kontinjenLelaki,
+    							'kontinjenWanita'	=> $kontinjenWanita,
+    							'peserta'			=> $peserta,
+    							'pesertaLelaki'		=> $pesertaLelaki,
+    							'pesertaWanita'		=> $pesertaWanita
+							]);
+    	}
+
+    	return view('members.summary', compact('collections'));
     }
 }
--- resources/views/members/summary.blade.php
+++ resources/views/members/summary.blade.php
@@ -4,69 +4,76 @@
 
 <div class="container-fluid">
 	
-	<div class="row">
-		<div class="col-xs-3 col-xs-offset-1">
-			<div class="panel panel-primary">
-				<div class="panel-heading"><h4>Kontinjen</h4></div>
-				<div class="panel-body">
-					
-				</div>
+	@foreach($collections as $collection)
+
+		@if($loop->iteration % 3 == 0)
+			<div class="row">
+				<div class="col-xs-3 col-xs-offset-1">
+					<div class="panel panel-primary">
+						<div class="panel-heading"><h4>{{ $collection['agensi'] }}</h4></div>
+						<div class="panel-body">
+
+							<table class="table">
+							<tr>
+								<td>Perkara</td>
+								<td>Lelaki</td>
+								<td>Wanita</td>
+								<td>Jumlah</td>
+							</tr>
+							<tr>
+								<td>Kontinjen</td>
+								<td>{{ $collection['kontinjenLelaki'] }}</td>
+								<td>{{ $collection['kontinjenWanita'] }}</td>
+								<td>{{ $collection['kontinjen'] }}</td>
+							</tr>
+							<tr>
+								<td>Peserta</td>
+								<td>{{ $collection['pesertaLelaki'] }}</td>
+								<td>{{ $collection['pesertaWanita'] }}</td>
+								<td>{{ $collection['peserta'] }}</td>
+							</tr>
+						</table>
+
+							
+						</div>
+					</div>
+
+				</div>	
 			</div>
+		@else
+			<div class="col-xs-3 col-xs-offset-1">
+				<div class="panel panel-primary">
+					<div class="panel-heading"><h4>{{ $collection['agensiKod'] }}</h4></div>
+					<div class="panel-body">
 
-		</div>
-		<div class="col-xs-3">
-			<div class="panel panel-primary">
-				<div class="panel-heading"><h4>Kontinjen</h4></div>
-				<div class="panel-body">
-					
+						<table class="table">
+							<tr>
+								<td>Perkara</td>
+								<td>Lelaki</td>
+								<td>Wanita</td>
+								<td>Jumlah</td>
+							</tr>
+							<tr>
+								<td>Kontinjen</td>
+								<td>{{ $collection['kontinjenLelaki'] }}</td>
+								<td>{{ $collection['kontinjenWanita'] }}</td>
+								<td>{{ $collection['kontinjen'] }}</td>
+							</tr>
+							<tr>
+								<td>Peserta</td>
+								<td>{{ $collection['pesertaLelaki'] }}</td>
+								<td>{{ $collection['pesertaWanita'] }}</td>
+								<td>{{ $collection['peserta'] }}</td>
+							</tr>
+						</table>
+
+						
+					</div>
 				</div>
+
 			</div>
-
-		</div>
-		<div class="col-xs-3">
-			<div class="panel panel-primary">
-				<div class="panel-heading"><h4>Kontinjen</h4></div>
-				<div class="panel-body">
-					
-				</div>
-			</div>
-
-		</div>
-
-		
-	</div>
-
-	<div class="row">
-		<div class="col-xs-3 col-xs-offset-1">
-			<div class="panel panel-primary">
-				<div class="panel-heading"><h4>Kontinjen</h4></div>
-				<div class="panel-body">
-					
-				</div>
-			</div>
-
-		</div>
-		<div class="col-xs-3">
-			<div class="panel panel-primary">
-				<div class="panel-heading"><h4>Kontinjen</h4></div>
-				<div class="panel-body">
-					
-				</div>
-			</div>
-
-		</div>
-		<div class="col-xs-3">
-			<div class="panel panel-primary">
-				<div class="panel-heading"><h4>Kontinjen</h4></div>
-				<div class="panel-body">
-					
-				</div>
-			</div>
-
-		</div>
-
-		
-	</div>
+		@endif
+	@endforeach
 
 </div>
 
